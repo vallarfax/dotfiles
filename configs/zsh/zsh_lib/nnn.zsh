9a217ee2231e4e108c392c9b12023f453eb16ad2
Chris McCollister <oaeui@me.com>
2021-07-01 19:03:33 -0500

    updates nnn with shift+q to cd on quit

--- configs/zsh/zsh_lib/nnn.zsh
+++ configs/zsh/zsh_lib/nnn.zsh
@@ -1,7 +1,33 @@
-alias n=nnn
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_PLUG='f:fzcd;p:preview-tui;j:fzz'
 
 BLK="00" CHR="00" DIR="04" EXE="01" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="00" FIFO="00" SOCK="00" OTHER="00"
 export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
 
+n ()
+{
+    # Block nesting of nnn in subshells
+    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The default behaviour is to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # To cd on quit only on ^G, remove the "export" as in:
+    #     NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    # NOTE: NNN_TMPFILE is fixed, should not be modified
+    NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    nnn "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
